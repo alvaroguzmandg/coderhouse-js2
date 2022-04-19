d53aa62355a3a55213e8a766031f3f23f2b7aa25
alvaroguzmandg <aguzman@VERSARDMC003.local>
2022-04-19 15:26:19 -0300

    Entrega Ajax y Fetch

---
+++ 15_AJAX_FETCH/clase13Guzman.js
@@ -0,0 +1,405 @@
+class ListadoProductos {
+    constructor(id, marca, modelo, talles, color, categoria, precio) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.talle = talles;
+        // this.stock = stock;
+        this.color = color;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    //Asignación de código de producto
+    asignarCodigo() {
+        let letraMarca = this.marca.charAt(0)
+        let letraMarca2 = this.marca.charAt(1)
+        let letraModelo = this.modelo.charAt(0)
+        let letraColor = this.color.charAt(0)
+        let letraCategoria = this.categoria.charAt(0)
+        this.codProducto = letraMarca + letraMarca2 + letraModelo + letraColor + letraCategoria;
+    }
+
+    //Validación de stock a la hora de elegir el producto
+    // validarStockProducto() {
+    //     if (this.stock > 0) {
+    //         return true
+    //     } else {
+    //         return false
+    //     }
+    // }
+
+    //Resta de stock disponible en caso de agregar al carrito
+    // restarStock() {
+    //     this.stock = this.stock - 1
+    // }
+
+    // // Suma de stock en caso de entrar más productos
+    // sumarStock() {
+    //     this.stock = this.stock + 1
+    // }
+}
+
+//Array de modelos de zapatillas existentes
+// let listadoProductos = []
+
+// async function mostrarProductos() {
+//     let productos = await fetch("/productos.json");
+
+//     listadoProductos = [await productos.json()]
+
+// }
+
+
+const definirProductos = async() => {
+    const resp = await fetch('productos.json')
+    listadoProductos = await resp.json()
+
+    listadoProductos.forEach(listadoProductos => {
+        let letraMarca = listadoProductos.marca.charAt(0)
+        let letraMarca2 = listadoProductos.marca.charAt(1)
+        let letraModelo = listadoProductos.modelo.charAt(0)
+        let letraColor = listadoProductos.color.charAt(0)
+        let letraCategoria = listadoProductos.categoria.charAt(0)
+        listadoProductos.codProducto = letraMarca + letraMarca2 + letraModelo + letraColor + letraCategoria;
+    })
+
+    mostrarProductos()
+    agregarAlCarrito()
+    listadoCarrito()
+}
+
+
+definirProductos()
+
+
+// DECLARACIÓN DE ARRAYS
+class CarroDeCompras {
+    constructor(id, marca, modelo, talles, color, cantidad, codProducto, imagen, precio) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.talle = talles;
+        this.color = color;
+        this.cantidad = cantidad;
+        this.codProducto = codProducto;
+        this.imagen = imagen;
+        this.precio = precio;
+    }
+    sumarCantidad() {
+        this.cantidad++
+    }
+}
+
+
+
+
+
+
+//Creación de Array con carrito Almacenado
+let carroDeComprasAlmacenado = JSON.parse(localStorage.getItem('carritoAlmacenado'))
+
+//Versión con operador ternario: Si la declaración del array con carrito Almacenado es nula, 
+//declara el carro de compras vacío y sino, crea el array de carro de compras concatenando el array de productos almacenados en el carrito
+// carroDeComprasAlmacenado === null ? carroDeCompras = [] : carroDeCompras = [].concat(carroDeComprasAlmacenado)
+
+//Versión con operador lógico OR de la declaración del array del carrito
+carroDeCompras = carroDeComprasAlmacenado || []
+
+//Variables que genera el contenedor de productos
+let contenidoProductos = document.getElementsByClassName("contenedorProductos");
+
+
+
+
+// DECLARACIÓN DE FUNCIONES
+
+//Función que genera el contenido del encabezado
+function infoEncabezado(nombreUsuario) {
+
+    let contenidoEncabezado = document.getElementsByClassName("datosUsuario")
+
+    spanNombre = document.createElement('span');
+    spanNombre.classList.add("nombreUsuario")
+    spanNombre.innerHTML = `¡Hola ${nombreUsuario}!` + ` / <div id="btnCerrarUsuario">Cerrar Sesión</div>`;
+
+    contenidoEncabezado[0].appendChild(spanNombre);
+
+
+    divCarro = document.getElementsByClassName("datosCarro");
+
+    divContenidoCarrito = document.createElement('span');
+    divContenidoCarrito.classList.add("accesoCarrito")
+
+    let contenidoCarrito = document.getElementsByClassName("contenedorCarrito")
+    contenidoCarrito[0].appendChild(divContenidoCarrito)
+    actualizarContadorCarro();
+}
+
+//Función de cerrar sesión
+function cerrarSesion() {
+    localStorage.clear();
+    location.reload();
+}
+
+
+//Lista los productos en el carrito
+function listadoCarrito() {
+    //Función para actualizar contador del carrito
+    actualizarContadorCarro();
+    //Se Actualiza el monto del carrito
+    precioCarrito();
+
+    //Revisa productos Almacenados en LocalStorage y las carga al Carrito
+    if (localStorage.getItem('carritoAlmacenado') != null) {
+        precioCarritoTotal = localStorage.getItem("Precio Carrito");
+        let contenedor = document.getElementById("productosEnCarrito");
+        contenedor.innerHTML = " "
+
+        htmlString = `
+        <span class="listadoCarrito">
+        <span class="listadoCarrito--titulo">LISTADO DE PRODUCTOS</span>`
+
+
+        for (let index = 0; index < carroDeCompras.length; index++) {
+
+            let marca = carroDeCompras[index].marca;
+            let modelo = carroDeCompras[index].modelo;
+            let talle = carroDeCompras[index].talle;
+            let precio = carroDeCompras[index].precio;
+            let imagen = carroDeCompras[index].imagen;
+            let id = carroDeCompras[index].id;
+            let cantidad = carroDeCompras[index].cantidad;
+            // let codProducto = carroDeCompras[index].codProducto
+
+            // String HTML de la vista del producto en el carrito
+            htmlString += `<li id="productoAgregado${id}">
+                    <span class="carrito__productoTexto">
+                        <span class="carrito__productoImagen"><img src="images/${imagen}.png"></span>
+                        <span class="carrito__productoTexto--titulo">${marca} ${modelo}
+                        <br>
+                        <span class="carrito__productoTexto--titulo">$${precio}
+                        <br>
+                        <span class="carrito__productoTexto--titulo">Cantidad: ${cantidad}
+                        <br>
+                        <span class="carrito__productoTexto--titulo">Talle: ${talle}
+                        <br>
+                        <span onclick="quitarProducto(${id})" class="carrito__productoTexto--quitar">Eliminar</span></span>
+                    </span>
+                    
+                    </li>
+                    `
+            contenedor.innerHTML = htmlString + `
+            
+            <span id="precioCarrito">Total: $${precioCarritoTotal}</span>
+            <span class="btRun-med" id="finalizarCompra">FINALIZAR COMPRA</span>
+            <span id="vaciarCarrito">Vaciar Carrito</span></span>
+            `
+            const botonVaciar = document.getElementById("vaciarCarrito");
+            botonVaciar.addEventListener("click", vaciarCarrito)
+        }
+
+    }
+}
+
+//Función para vaciar el carrito
+function vaciarCarrito() {
+    carroDeCompras.length = 0;
+    localStorage.setItem("carritoAlmacenado", JSON.stringify(carroDeCompras));
+    listadoCarrito();
+    //Se Actualiza el monto del carrito
+    precioCarrito();
+    Toastify({
+        text: "¡Carrito eliminado!",
+        duration: 4000,
+        style: {
+            with: "300px",
+            background: "red",
+        },
+    }).showToast();
+}
+
+//Función para mostrar el carrito con mouseover
+let botonCarrito = document.getElementById("carroDeCompras")
+botonCarrito.onmouseover = () => {
+    let contenedorCarrito = document.getElementById("productosEnCarrito")
+    contenedorCarrito.style.display = "block";
+}
+botonCarrito.onmouseout = () => {
+    let contenedorCarrito = document.getElementById("productosEnCarrito")
+    contenedorCarrito.style.display = "none";
+}
+
+
+//Función para quitar productos del carrito
+function quitarProducto(id) {;
+
+
+    const item = carroDeCompras.find((prod) => prod.id === id)
+    const indice = carroDeCompras.indexOf(item)
+    carroDeCompras.splice(indice, 1)
+
+    localStorage.setItem("carritoAlmacenado", JSON.stringify(carroDeCompras));
+    listadoCarrito();
+    //Se Actualiza el monto del carrito
+    precioCarrito()
+
+    Toastify({
+        text: "¡Producto eliminado!",
+        duration: 2000,
+        style: {
+            background: "red",
+        },
+    }).showToast();
+}
+
+//Función para actualizar el número de productos en el carrito
+function actualizarContadorCarro() {
+    cantidad = 0;
+    for (let i = 0; i < carroDeCompras.length; i++) {
+        let suma = carroDeCompras[i].cantidad
+        cantidad = cantidad + suma
+    }
+    divContenidoCarrito.innerHTML = `Productos en el carrito: ${cantidad}`;
+}
+
+
+//Función para mostrar los productos disponibles en el sitio
+function mostrarProductos() {
+
+    listadoProductos.forEach((listadoProductos) => {
+
+        let divProducto = document.createElement('div');
+        divProducto.classList.add("bloqueProducto");
+
+        htmlString = `<span class="productoFoto"><img src="images/${listadoProductos.marca.charAt(0)}${listadoProductos.modelo.charAt(0)}${listadoProductos.color.charAt(0)}.png"></span>
+        <span class="productoMarca">${listadoProductos.marca} ${listadoProductos.modelo} </span>
+        <span class="productoDescripcion">Categoria: ${listadoProductos.categoria} / Color: ${listadoProductos.color} </span>
+        <span class="productoDescripcion">Precio $${listadoProductos.precio}</span>
+        <span class="productoEspecificaciones">
+            <select id="talles-${listadoProductos.codProducto}" class="caja" value="Elegí tu talle">
+            <option value="Elegí tu talle">Talle</option>`;
+
+        let talleProducto = listadoProductos.talle
+        for (let i = 0; i < talleProducto.length; i++) {
+            let valor = `${listadoProductos.talle[i]}`
+
+            let option1 = document.createElement("option");
+            let select = document.getElementById("talles")
+
+            option1.setAttribute("value", valor);
+            let option1Texto = document.createTextNode("opcion 1");
+            htmlString += `<option value="${valor}">${valor}</option>`
+        }
+
+        htmlString += `</select>
+            <button id="btnCompra-${listadoProductos.id}" class="btnAgregar">AGREGAR</button>
+            </span>
+            `
+        htmlString += "</ul>"
+        divProducto.innerHTML = htmlString
+        contenidoProductos[0].append(divProducto);
+
+
+
+    })
+}
+
+
+//Función para Agregar productos al carrito
+function agregarAlCarrito() {
+
+    listadoProductos.forEach((listadoProductos) => {
+
+        let btnAgregar = document.getElementById(`btnCompra-${listadoProductos.id}`);
+        btnAgregar.addEventListener("click", () => {
+            let talleCompra = document.getElementById(`talles-${listadoProductos.codProducto}`).value;
+            const { id, marca, modelo, color, codProducto, precio } = listadoProductos
+            let cantidad = 1;
+            let codCompra = codProducto + talleCompra
+            let imagen = marca.charAt(0) + modelo.charAt(0) + color.charAt(0);
+            let idComprado = id;
+
+            if (talleCompra == "Elegí tu talle") {
+                // alert("Por favor, elegí un talle")
+                Swal.fire({
+                        title: '¡Atención!',
+                        text: 'Por favor, elegí un talle',
+                        icon: 'question',
+                        confirmButtonText: '¡Elegir!',
+                        color: '#FF5000',
+                        background: '#FF5500',
+                        backdrop: 'rgba(0, 0, 0, 0.8)',
+                        // ANIMACIÓN
+                        showClass: {
+                            popup: 'animate__animated animate__fadeInDown'
+                        },
+                        hideClass: {
+                            popup: 'animate__animated animate__fadeOutUp'
+                        },
+
+                    })
+                    // .then((result) => {})
+            } else {
+
+                const existe = carroDeCompras.some(carroDeCompras => carroDeCompras.codProducto === codCompra)
+
+                if (existe) {
+                    const producto = carroDeCompras.map(carroDeCompras => {
+                        if (carroDeCompras.codProducto === codCompra) {
+                            carroDeCompras.cantidad++
+                        }
+                    })
+                } else {
+                    carroDeCompras.push(new CarroDeCompras(idComprado, marca, modelo, parseInt(talleCompra), color, cantidad, codCompra, imagen, precio));
+                }
+
+
+                localStorage.setItem("carritoAlmacenado", JSON.stringify(carroDeCompras));
+
+                Toastify({
+                    text: "¡Producto agregado al carrito!",
+                    duration: 3000,
+                    gravity: "bottom",
+                    style: {
+                        background: "#FF5500",
+                    }
+                }).showToast();
+
+
+                //Se actualizado el listado de productos en el carrito
+                listadoCarrito();
+                //Se Actualiza el contador del carrito
+                actualizarContadorCarro();
+                //Se Actualiza el monto del carrito
+                precioCarrito()
+            }
+
+        })
+    })
+
+}
+
+
+infoEncabezado(nombreUsuario)
+
+
+
+
+//CREAR SESIÓN
+let btnCerrarSesion = document.getElementById("btnCerrarUsuario")
+btnCerrarSesion.onclick = () => {
+    cerrarSesion();
+}
+
+
+//PRECIO CARRITO
+precioCarrito = () => {
+    let precioAcumulado = 0;
+    for (let i = 0; i < carroDeCompras.length; i++) {
+        let precioProducto = carroDeCompras[i].precio * carroDeCompras[i].cantidad
+        precioAcumulado += precioProducto
+    }
+    precioCarritoTotal = precioAcumulado
+    localStorage.setItem("Precio Carrito", precioCarritoTotal);
+}
---
+++ 15_AJAX_FETCH/login.js
@@ -0,0 +1,70 @@
+if (localStorage.getItem("usuario") == null) {
+    document.getElementById("formularioInicio").style.display = "none"
+    let parrafo = document.createElement("h3");
+    parrafo.innerHTML = `Registrate para ingresar`
+    document.getElementById("tituloRegistro").appendChild(parrafo);
+} else {
+    let textoTitulo = document.createElement("h3");
+    usuario = localStorage.getItem("usuario");
+    let parrafo = document.createElement("h2");
+    parrafo.innerHTML = `Iniciar sesion`
+        // document.getElementById("tituloSesion").appendChild(parrafo);
+        // document.getElementById("tituloSesion").appendChild(textoTitulo);
+    document.getElementById("formularioRegistro").style.display = "none";
+}
+
+
+
+//REGISTRARSE
+let formularioRegistro = document.getElementById("formularioRegistro");
+formularioRegistro.addEventListener("submit", registrarse);
+
+
+function registrarse(event) {
+    event.preventDefault();
+    let elemento = event.target;
+    console.log(elemento.children)
+    email = elemento.children[1].value;
+    usuario = elemento.children[2].value;
+    pass = elemento.children[3].value;
+    localStorage.setItem("email", email);
+    localStorage.setItem("usuario", usuario);
+    localStorage.setItem("contraseña", pass);
+    localStorage.setItem("log", "true");
+    window.location.replace("tienda-clase13Guzman.html");
+}
+
+
+let btnCrearUsuario = document.getElementById("btnCrearUsuario")
+btnCrearUsuario.onclick = () => {
+    crearSesion();
+}
+
+//Función de cerrar sesión
+function crearSesion() {
+    localStorage.clear();
+    location.reload();
+}
+
+let formularioInicio = document.getElementById("formularioInicio");
+formularioInicio.addEventListener("submit", loguearse);
+
+
+//VALIDACIÓN DE CONTRASEÑA
+function loguearse(e) {
+    e.preventDefault();
+    let elemento = e.target;
+    usuarioIngresado = elemento.children[1].value;
+    passIngresada = elemento.children[2].value;
+    nombre = localStorage.getItem("usuario")
+    pass = localStorage.getItem("contraseña")
+    if ((usuarioIngresado == nombre) && (passIngresada == pass)) {
+        localStorage.setItem('log', "true");
+        window.location.replace("tienda-clase13Guzman.html");
+    } else {
+        let errorPass = document.createElement("span");
+        errorPass.innerHTML = `USUARIO O CONTRASEÑA INCORRECTO`
+        errorPass.classList.add("errorPass");
+        document.getElementById("formularioInicio").appendChild(errorPass);
+    }
+}
